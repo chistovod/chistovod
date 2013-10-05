6ef568f4c1788e59f554e95ca5b4839b6956d55f
Marat Zaynutdinov <tsundokum@gmail.com>
2013-10-05 21:37:50 +0400

    db is named zakupki.sqlite added *.sqlite to .gitignore

--- ChistovodAnalytics/views.py
+++ ChistovodAnalytics/views.py
@@ -1,8 +1,14 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from controllers import parse_and_save_notifications
+from django.core import serializers
+from models import NotificationOK
 
 
 def parse(request):
     parse_and_save_notifications()
     return render_to_response('parse.html')
+
+
+def notificationOK(request):
+    return render_to_response('notificationOK.html', {'data':NotificationOK.objects.all()})
--- chistovod/urls.py
+++ chistovod/urls.py
@@ -14,6 +14,8 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^parse/$', 'ChistovodAnalytics.views.parse')
+    url(r'^parse/$', 'ChistovodAnalytics.views.parse'),
+
+    url(r'^notificationOK/$', 'ChistovodAnalytics.views.notificationOK')
 )
 

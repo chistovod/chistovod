2db91621533ea61ed8adbc4912a95f34a9f2e156
kalaidin <pavel.kalaidin@gmail.com>
2013-10-06 00:37:40 +0400

    Show max prices of notifications.

--- ChistovodAnalytics/views.py
+++ ChistovodAnalytics/views.py
@@ -12,3 +12,7 @@ def parse(request):
 
 def notificationOK(request):
     return render_to_response('notificationOK.html', {'data':NotificationOK.objects.all()})
+
+
+def plot(request):
+    return render_to_response('plot.html', {'data': NotificationOK.objects.all()})

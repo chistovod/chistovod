9462538afe3b5c1144c0c74360d98e4052556260
Marat Zaynutdinov <tsundokum@gmail.com>
2013-10-05 19:59:22 +0400

    notificationOK model

--- ChistovodAnalytics/models.py
+++ ChistovodAnalytics/models.py
@@ -1,21 +1,13 @@
 from django.db import models
 
-class Poll(models.Model):
-    question = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    
-class Choice(models.Model):
-    poll = models.ForeignKey(Poll)
-    choice = models.CharField(max_length=200)
-    votes = models.IntegerField()
-    
+
 class NotificationOK(models.Model):
     id = models.IntegerField
     notification_number = models.CharField(max_length=19)
-    create_date
-    publish_date
-    order_name
-    href
-    reg_num
-    max_price
+    create_date = models.DateTimeField()
+    publish_date = models.DateTimeField()
+    order_name = models.CharField(max_length=200)
+    href = models.CharField(max_length=200)
+    reg_num = models.IntegerField()
+    max_price = models.FloatField()
 

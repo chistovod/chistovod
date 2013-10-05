4826b66b5f1a48be9a0d86007c27e388059f6a4c
Timur Latypoff <n.litened@gmail.com>
2013-10-06 02:46:20 +0400

    Added db_index to all searchable fields of models

--- ChistovodAnalytics/models.py
+++ ChistovodAnalytics/models.py
@@ -2,32 +2,32 @@ from django.db import models
 
 
 class Lot(models.Model):
-    notification_number = models.CharField(max_length=19)
-    create_date = models.DateTimeField()
-    publish_date = models.DateTimeField()
+    notification_number = models.CharField(max_length=19, db_index=True)
+    create_date = models.DateTimeField(db_index=True)
+    publish_date = models.DateTimeField(db_index=True)
     notification_name = models.CharField(max_length=200)
     lot_name = models.CharField(max_length=200)
     href = models.CharField(max_length=200)
-    max_price = models.FloatField()
-    reg_num = models.BigIntegerField()
-    ordinal_number = models.IntegerField()
-    final_price = models.FloatField(null=True)
-    contract_sign_date = models.DateTimeField(null=True)
-    execution_date = models.DateTimeField(null=True)
+    max_price = models.FloatField(db_index=True)
+    registration_number = models.BigIntegerField(db_index=True)
+    ordinal_number = models.IntegerField(db_index=True)
+    final_price = models.FloatField(null=True, db_index=True)
+    contract_sign_date = models.DateTimeField(null=True, db_index=True)
+    execution_date = models.DateTimeField(null=True, db_index=True)
 
 
 class Customer(models.Model):
-    registration_number = models.BigIntegerField()
-    inn = models.BigIntegerField()
-    okato = models.IntegerField()
+    registration_number = models.BigIntegerField(db_index=True)
+    inn = models.BigIntegerField(db_index=True)
+    okato = models.IntegerField(db_index=True)
     name = models.CharField(max_length=200)
 
 
 class Supplier(models.Model):
-    inn = models.CharField(max_length=100)
+    inn = models.CharField(max_length=100, db_index=True)
     participant_type = models.CharField(max_length=100)
     organization_form = models.CharField(max_length=100)
-    organization_name = models.CharField(max_length=100)
+    organization_name = models.CharField(max_length=200)
 
 
 class Contact(models.Model):

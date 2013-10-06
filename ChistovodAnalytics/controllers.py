f463386892f174891023625f136a47feb1e20be7
Marat Zaynutdinov <tsundokum@gmail.com>
2013-10-06 13:18:59 +0400

    parse files in a given strick order

--- ChistovodAnalytics/controllers.py
+++ ChistovodAnalytics/controllers.py
@@ -13,6 +13,9 @@ from zakupki_xml_parser import *
 VALID_NOTIFICATIONS = re.compile('\}notification(OK|EF|ZK|PO)$')
 VALID_PROTOCOLS = re.compile('\}protocol(OK1|EF3|ZK1|ZK5|PO1)$')
 
+VALID_FILE_PREFIXI = ['organization', 'notification', 'protocol', 'contract']
+ORDERS = tuple(enumerate(VALID_FILE_PREFIXI))
+
 
 def parse_file(f):
     for event, xml in etree.iterparse(f, huge_tree=True):
@@ -25,7 +28,7 @@ def parse_file(f):
             #Customer(**cust_dict).save()
             print cust_dict
         elif VALID_PROTOCOLS.search(str(xml.tag)):
-            suppliers, contacts = read_suppliers_and_contacts_from_protocols(xml)
+            suppliers, contacts, supplier_to_lot = read_suppliers_and_contacts_from_protocols(xml)
             #Supplier
             #Contact
             print suppliers
@@ -33,10 +36,11 @@ def parse_file(f):
 
 
 def process_file(f, filename):
-    # ignoring non-notifications for now #####
-    if filename.find('notification') < 0 and filename.find('organization') < 0:
+
+    # process only files with valid prefix
+    if all([filename.find(prefix) == -1 for prefix in VALID_FILE_PREFIXI]):
         return
-        ##########################################
+    ##########################################
 
     if filename.endswith('.xml'):
         print "Parsing file", filename
@@ -54,13 +58,25 @@ def process_any_file(file):
             process_file(f, file)
 
 
-def process_all_files():
-    path = expanduser('~') + '/zakupki.gov.ru'
+def get_file_parse_order(file_name):
+    fn = file_name.lower()
+    for order, prefix in ORDERS:
+        if fn.startswith(prefix):
+            return order
+    return len(ORDERS)
 
+
+def os_flat_walk(path):
     for root, subfolders, files in os.walk(path):
-        for file in files:
-            filepath = os.path.join(root, file)
-            process_any_file(filepath)
+        for f in files:
+            filepath = os.path.join(root, f)
+            yield (filepath, get_file_parse_order(f))
+
+
+def process_all_files():
+    path = os.path.join(expanduser('~'), 'zakupki.gov.ru')
+    for filepath, order in sorted(os_flat_walk(path), key=itemgetter(1)):
+        process_any_file(filepath)
 
 
 if __name__ == "__main__":
